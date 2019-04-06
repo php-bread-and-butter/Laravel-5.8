e25310c481d680dfc1f165c1c4d546021945e29a
Subhadeep Chowdhury <mipl.subhadeep@gmail.com>
2019-04-06 20:55:41 +0530

    First commit

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Jason Robinson',
+            'email' => 'jason@gmail.com',
+            'password' => bcrypt('Jason@123')
+        ]);
+    }
+}
